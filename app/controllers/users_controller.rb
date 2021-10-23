107e135dc461f8a44ff9facb83d93d22f608ce38
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-10-23 11:49:06 +0000

    [create]controller homes users reports

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def withdrawal
+  end
+
+  def withdrawal_update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'reports/new'
+  get 'reports/show'
+  get 'reports/edit'
+  get 'reports/update'
+  get 'reports/destroy'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/withdrawal'
+  get 'users/withdrawal_update'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
