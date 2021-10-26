baf4df65abdd35cd6ce64878fdd45e3b8633f8b7
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-10-26 09:46:58 +0000

    [edit]routes.rb [fix]links

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   get '/'      => "homes#top"   , as: "top"
   get '/about' => "homes#about" , as: "about"
 
-  resource :users, only: [:show, :edit, :update]
-  get   'users/withdrawal'          => "users#withdrawal" , as: "withdrawal"
-  patch 'users/withdrawal'   => "users#withdrawal" , as: "withdrawal_update"
+  get   'user/show'          => "users#show"       , as: "users_show"
+  get   'user/edit'          => "users#edit"       , as: "users_edit"
+  patch 'user/edit'        => "users#update"     , as: "users_update"
+  get   'user/withdrawal'   => "users#withdrawal" , as: "withdrawal"
+  patch 'user/withdrawal'   => "users#withdrawal" , as: "withdrawal_update"
 
   resource :reports, only: [:new, :show, :edit, :update, :destroy]
   get   'reports/index'          => "reports#index" , as: "reports_index"
